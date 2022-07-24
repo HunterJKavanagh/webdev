b380a7ee645c862b0460a9fd5dbb342bf1b18007
Hunter Kavanagh <hunter.kavanagh@protonmail.com>
2022-07-24 14:30:41 -0500

    lab10

---
+++ lab10/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import BodyComponent from './BodyComponent';
+
+function App() {
+  return (
+    <div className="App-header">
+      <header>
+        <h1>Kavanagh - Lab 10 react App</h1>
+      </header>
+      <BodyComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab10/src/BodyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BodyComponent = () => {
+    return (
+        <main>
+            <p>All those Kit Kat bars that are somehow made imperfect during production—due to air bubbles, weirdly shaped wafers, or some other issue—aren't tossed out, but instead ground up into a fine paste and turned into a filling that's then incorporated back into the production process to create new Kit Kat bars. It's the circle of candy life.</p>
+        </main>
+    )
+}
+
+export default BodyComponent;
